aa4924e1397f283ebc2faa68a3fe485c50bbe169
Michael Karrer <michaelkarrer81@gmail.com>
2017-08-03 00:33:18 +0200

    [ADD] __openerp__.py for the default instance addon

---
+++ addons/rnga_config/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'rnga_config',
+    'summary': '''FS-Online rnga instance configuration''',
+    'description': '''
+FS-Online Instance Configuration
+================================
+
+Customer configuration for the instance rnga
+
+- Default settings
+- View modifications
+- CSS
+- Translations
+    ''',
+    'author': 'Michael Karrer (michael.karrer@datadialog.net), DataDialog',
+    'version': '1.0',
+    'website': 'https://www.datadialog.net',
+    'installable': True,
+    'depends': [
+        'fsonline',
+    ],
+    'data': [],
+}
+
+    
